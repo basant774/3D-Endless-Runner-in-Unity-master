da44ffc4d67f3e8ce038ca70c783ec4aac009328
basant774 <117537117+basant774@users.noreply.github.com>
2024-05-13 01:43:25 +0300

    Initial commit

---
+++ EndlessRunnerTutorial/Assets/Scripts/Coin.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Coin : MonoBehaviour {
+   //  [SerializeField] Text Text2;
+
+    [SerializeField] float turnSpeed = 90f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Obstacle>() != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Check if the object we collided with is the player
+        if (other.gameObject.name != "Player")
+        {
+            return;
+        }
+
+        // Add to the player's score
+        GameManager.inst.IncrementScore();
+
+        // Check if the player's score has reached 10
+        if (GameManager.inst.score >= 10)
+        {
+           // Text2.text = "Congratulations! You're going to Level 2";
+
+            // Load level 2
+            Application.LoadLevel("Level2");
+        }
+
+        // Destroy this coin object
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0, 0, turnSpeed * Time.deltaTime);
+    }
+}
---
+++ EndlessRunnerTutorial/Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoad : MonoBehaviour
+{
+        public int score = 0; // تحديد متغير score
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.G)){
+            Application.LoadLevel("Level2");
+        }
+        
+    
+    if (score == 3)
+        {
+            // إذا كانت قيمة score تساوي 3، قم بتحميل المستوى القادم هنا
+            LoadNextLevel();
+        }
+    }
+
+    // دالة لتحميل المستوى القادم
+    void LoadNextLevel()
+    {
+        // يمكنك هنا تحديد اسم المستوى القادم للانتقال إليه بعد الحصول على النقاط اللازمة
+        string nextLevelName = "Level2"; // افترضنا أن اسم المستوى القادم هو "Level3"
+        Application.LoadLevel(nextLevelName); // قم بتحميل المستوى القادم
+    }
+}
